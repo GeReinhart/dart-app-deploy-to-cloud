1c217b0523fe4024fa0690f8f3508d53517429dd
Gerald Reinhart <gerald.reinhart@gmail.com>
2014-01-02 23:11:30 +0100

    get and test

---
+++ prepareHerokuDeployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+get-dart-dependencies
+test-dart-sources
+
+
+
+
+function get-dart-dependencies {
+   echo "get-dart-dependencies"
+   pub get
+}
+
+function test-dart-sources {
+   echo "test-dart-sources"
+   dart test/model_tests.dart
+}
