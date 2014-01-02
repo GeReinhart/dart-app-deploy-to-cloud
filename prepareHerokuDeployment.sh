eecde8180135e263c415a6e2d6a6890468758315
Gerald Reinhart <gerald.reinhart@gmail.com>
2014-01-02 23:56:51 +0100

    commit changes

--- prepareHerokuDeployment.sh
+++ prepareHerokuDeployment.sh
@@ -19,11 +19,25 @@ function dart-2-js {
 	for clientFile  in $clientFiles
 	do
             echo "dart2js on $clientDir/$clientFile"
-	    dart2js "$clientDir/$clientFile" -o "$clientDir/$clientFile.js"
+	    dart2js "$clientDir/$clientFile" -o "$clientDir/$clientFile.js" || { echo 'dart-2-js failed' ; exit 1; }
 	done
+}
+
+
+function commit-changes {
+
+	echo "commit-changes"
+	git config --global user.email "support@drone.io"
+	git config --global user.name "Drone Server"
+	git status 
+	git add .
+	git commit -m "add compiled and prefixed files" || { echo 'commit-changes' ; exit 1; }
+
 }
 
 get-dart-dependencies 
 test-dart-sources
 dart-2-js
+commit-changes
+
 
