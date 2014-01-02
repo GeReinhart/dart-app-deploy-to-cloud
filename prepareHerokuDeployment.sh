ef3eb9870c17024f7d54b7d26ec76abae3671a75
Gerald Reinhart <gerald.reinhart@gmail.com>
2014-01-02 23:42:39 +0100

    test fix and dart2js

--- prepareHerokuDeployment.sh
+++ prepareHerokuDeployment.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 function get-dart-dependencies {
-   echo "get-dart-dependencies"
-   pub get
+	echo "get-dart-dependencies"
+	pub get || { echo 'get-dart-dependencies failed' ; exit 1; }
 }
 
 function test-dart-sources {
-   echo "test-dart-sources"
-   dart test/model_tests.dart
+	echo "test-dart-sources"
+	dart test/model_tests.dart || { echo 'test-dart-sources failed' ; exit 1; }
 }
 
+function dart-2-js {
 
+	echo "dart-2-js"
+	clientDir="web/client"
+	clientFiles=`grep -r "main()" $clientDir   | grep -v packages |  awk -F":" '{print $1}' | awk -F"/" '{print $3}' `
 
-get-dart-dependencies
+	for clientFile  in $clientFiles
+	do
+            echo "dart2js on $clientDir/$clientFile"
+	    dart2js "$clientDir/$clientFile" -o "$clientDir/$clientFile.js"
+	done
+}
+
+get-dart-dependencies 
 test-dart-sources
-
+dart-2-js
 
--- test/model_tests.dart
+++ test/model_tests.dart
@@ -4,7 +4,8 @@ import '../lib/model.dart';
 main() {
   
   test('blog link', () {
-    expect(new BlogLink("gereinhartasdartisan.blogspot.com","dart-automatic-deployement-in-the-cloud").link, equals("rando")); 
+    expect(new BlogLink("gereinhartasdartisan.blogspot.com","dart-automatic-deployement-in-the-cloud").link,
+        equals("http://gereinhartasdartisan.blogspot.com/dart-automatic-deployement-in-the-cloud")); 
   });
   
 }
